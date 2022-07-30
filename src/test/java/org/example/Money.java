ebdfcc229ea22bcf1bcb836116ca29a0657a8f59
murat.sokucu <murat.sokucu@trendyol.com>
2022-07-30 14:35:42 +0300

    ch2: degenerate objects (Dollar side affects?)

--- src/test/java/org/example/Money.java
+++ src/test/java/org/example/Money.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.testng.Assert;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class Money {
@@ -16,16 +16,15 @@ public class Money {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(five.amount, 10);
+        Dollar product = five.times(2);
+        assertEquals(product.amount, 10);
+        product = five.times(3);
+        assertEquals(product.amount, 15);
     }
 
     /**
-     * 1- Made a list of the tests we knew we needed to have working
-     * 2- Told a story with a snippet of code about how we wanted to view one operation
-     * 3- Made the test compile with stubs
-     * 4- Made the test run by committing horrible sins
-     * 5- Gradually generalized the working code, replacing constants with variables
-     * 6- Added items to our to-do list rather than addressing them all at once
+     * Two of three strategies for quickly getting to green
+     * 1- Fake it -> Return a constant and gradually replace constants with variable until you have the real code.
+     * 2- Use to obvious implementation -> Type in the real implementation.
      */
 }
