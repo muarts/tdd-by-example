63537fe5fd4d5d97bf0a06d6d2571e29deafc7e8
murat.sokucu <murat.sokucu@trendyol.com>
2022-07-31 20:30:37 +0300

    ch6: equality for all, redux

--- src/test/java/org/example/MoneyTest.java
+++ src/test/java/org/example/MoneyTest.java
@@ -12,15 +12,13 @@ public class MoneyTest {
         assertEquals(five.times(2), new Dollar(10));
         assertEquals(five.times(3), new Dollar(15));
     }
-
-    /**
-     * Triangulation: We only generalize (finding a general solution) code
-     * when we have two examples or more.
-     */
+    
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 
     @Test
