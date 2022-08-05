831db42232302493e3a7eedd6c1479dbda4d391b
murat.sokucu <murat.sokucu@trendyol.com>
2022-08-05 20:28:01 +0300

    ch11: delete subclasses Dollar and Franc

--- src/test/java/org/example/MoneyTest.java
+++ src/test/java/org/example/MoneyTest.java
@@ -12,13 +12,11 @@ public class MoneyTest {
         assertEquals(five.times(2), Money.dollar(10));
         assertEquals(five.times(3), Money.dollar(15));
     }
-    
+
     @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.dollar(5).equals(Money.franc(5)));
     }
 
@@ -33,10 +31,5 @@ public class MoneyTest {
     public void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
